29b14116250d349c3ce7b69d3e84b6acf53fd1df
shlomo <shlomo2096@gmail.com>
2025-07-10 15:52:59 +0300

    commit2

--- data_loader.py
+++ data_loader.py
@@ -5,3 +5,15 @@ class DataLoader:
     def load_data(file_path):
         return pd.read_csv(file_path)
 
+    @staticmethod
+    def load_json(file_path):
+        return pd.read_json(file_path)
+
+    @staticmethod
+    def load_excel(file_path):
+        return pd.read_excel(file_path)
+
+    @staticmethod
+    def load_from_df(df: pd.DataFrame):
+        return df
+
--- naive_bayes_classifier.py
+++ naive_bayes_classifier.py
@@ -1,52 +1,46 @@
-import numpy as np
-
-import model
 class NaiveBayesClassifier:
     def __init__(self):
         self.model = {}
-        self.prior_1 = 0
-        self.prior_2 = 0
+        self.priors = {}
+        self.classes = []
+
 
     def fit(self, df, target_column):
         target_variable = df[target_column]
         feature_cols = df.drop(columns=[target_column],axis=1)
-        unique_target_variable = target_variable.unique()
-        print(type(unique_target_variable))
-        prior = df[target_column].value_counts(normalize=True).to_dict()
-        # self.prior_yes = prior[0]
-        # self.prior_no = prior[1]
+        self.classes = target_variable.unique()
+        self.priors = df[target_column].value_counts(normalize=True).to_dict()
+
 
         for col in feature_cols:
             self.model[col] = {}
             unique_values = feature_cols[col].unique()
             for value in unique_values:
-                yes_count = len(df[(df[col] == value) & (target_variable == unique_target_variable[0])])
-                no_count = len(df[(df[col] == value) & (target_variable == unique_target_variable[0])])
-                total_yes = (target_variable == unique_target_variable[0]).sum()
-                total_no = (target_variable == unique_target_variable[1]).sum()
+                self.model[col][value] = {}
+                yes_count = len(df[(df[col] == value) & (target_variable == self.classes[0])]) +1
+                no_count = len(df[(df[col] == value) & (target_variable == self.classes[1])]) +1
+                self.model[col][value] = {self.classes[0]: yes_count, self.classes[1]: no_count}
 
-                self.model[col][value] = {
-                    'P(YES|X)': yes_count / total_yes,
-                    'P(NO|X)': no_count / total_no
-                }
+                total_yes = (target_variable == self.classes[0]).sum() + len(unique_values)
+                total_no = (target_variable == self.classes[1]).sum() + len(unique_values)
 
-        print(self.model)
+                self.model[col][value][f'P({self.classes[0]}|X)'] = yes_count / total_yes
+                self.model[col][value][f'P({self.classes[1]}|X)'] = no_count / total_no
+
 
     def classify(self, record):
-        prob_yes = self.prior_1
-        prob_no = self.prior_2
+        prob_yes = self.priors[self.classes[0]]
+        prob_no = self.priors[self.classes[1]]
 
         for col in record:
             value = record[col]
             if value in self.model[col]:
-                prob_yes *= self.model[col][value]['P(YES|X)']
-                prob_no *= self.model[col][value]['P(NO|X)']
-
-        if prob_yes > prob_no:
-            return 'yes'
-        else:
-            return 'no'
+                prob_yes *= self.model[col][value][f'P({self.classes[0]}|X)']
+                prob_no *= self.model[col][value][f'P({self.classes[1]}|X)']
+        # print(f"Probabilities: P({self.classes[0]}|X) = {prob_yes}, P({self.classes[1]}|X) = {prob_no}")
+        return self.classes[0] if prob_yes > prob_no else self.classes[1]
 
 
-a = NaiveBayesClassifier()
-a.fit(model.df,'p')
+
+
+
